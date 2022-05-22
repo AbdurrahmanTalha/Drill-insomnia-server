2fabc2530453a3861ca7a64a3fa7fd2e28c9c2d6
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-05-22 15:45:06 +0600

    added my order

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config()
@@ -38,12 +38,14 @@ async function run() {
         const drillCollection = client.db("drill-insomnia").collection("drills");
         const ratingCollection = client.db("drill-insomnia").collection("ratings");
         const usersCollection = client.db("drill-insomnia").collection("users");
+        const purchaseCollection = client.db("drill-insomnia").collection("purchase");
 
         app.get('/tools/home', async (req, res) => {
             const query = {}
             const tools = await drillCollection.find(query).limit(6).toArray()
             res.send(tools)
         })
+        
         app.get('/rating', async (req, res) => {
             const query = {}
             const tools = await ratingCollection.find(query).toArray()
@@ -62,6 +64,53 @@ async function run() {
                 expiresIn: "1d"
             })
             res.send({ result, accessToken: token })
+        })
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await drillCollection.findOne(query);
+            res.send(item);
+        });
+        app.post('/rating', verifyJWT, async (req, res) => {
+            const newReview = req.body;
+            const result = ratingCollection.insertOne(newReview)
+            res.send(result)
+        })
+        app.post("/purchase", async (req, res) => {
+            const purchase = req.body;
+            const result = await purchaseCollection.insertOne(purchase)
+            return res.send({ success: true, result: result })
+        })
+        app.put("/drill/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedQuantity = req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedQuantity.quantity
+                },
+            };
+            const result = drillCollection.updateOne(filter, updatedDoc, options);
+            res.send({ result })
+        })
+        app.get('/myOrder', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { buyerEmail: email };
+                const drill = await purchaseCollection.find(query).toArray();
+                res.send(drill);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
         })
     }
     finally {
