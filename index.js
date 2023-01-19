1ee02fbf429627509bc167e981d5170ec87569c0
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-01-19 20:13:33 +0600

    updating some stuff

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // Middleware
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvxit.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqqxbpw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const verifyJWT = (req, res, next) => {
